1765eb9d101072046a2f5824e4bfe80e963790a2
vivekkothawale <vivekkothawale95@gmail.com>
2017-05-23 09:21:08 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,11 @@
+n=int(input('enter number of days in month (28-31):'))
+d=int(input('start of day:'))
+i=1
+for i in range(1,n+1):
+    i<=n
+    print(i,end=" ")
+    if(i+d)%7 == 0:
+        print(d*"")
+        i = i+1
+
+
